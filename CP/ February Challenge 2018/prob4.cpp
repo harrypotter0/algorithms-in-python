ece5f55e702cdb37f5f04172895c4805cbfae57b
harrypotter0 <9654263057akashkandpal@gmail.com>
2018-02-14 16:01:13 +0530

    CP Season is Back

--- CP/ February Challenge 2018/prob4.cpp
+++ CP/ February Challenge 2018/prob4.cpp
@@ -13,13 +13,22 @@
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define MOD 1000000007
+#define gc getchar_unlocked
+#define ms(s, n) memset(s, n, sizeof(s))
+#define prec(n) fixed<<setprecision(n)
+typedef long long ll;
 
 int main() {
 	// your code goes here
 	#ifdef JUDGE
     freopen("input.txt", "rt", stdin);
     freopen("output.txt", "wt", stdout);
-    #endif
+  #endif
 	ios_base::sync_with_stdio(0);
 	cin.tie(NULL);
 	cout.tie(NULL);
@@ -27,24 +36,14 @@ int main() {
 	cin>>t;
 	while(t--)
 	{
-	    int n,i;
-	    long double c,d,s;
-	    cin>>n;
-	    long double arr[n],sum=0;
-	    for(i=0;i<n;i++){cin>>arr[i];sum+=arr[i];}
-	    cin>>c>>d>>s;
-	    long double t1=sum,t2=0;
-	    long double wait[n];
-	    wait[0]=arr[0];
-	    long double totwait=0,zero=0,curr=0;
-	    totwait+=wait[0];
-	    for(i=1;i<n;i++)
-	    {
-	        wait[i]=arr[i]-arr[i-1]+ (wait[i-1]<0?wait[i-1]:0);
-	        totwait+=max(wait[i],zero);
-	    }
-	    cout<<fixed<<setprecision(10)<<(c-1)*totwait<<endl;
+		int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)cin>>a[i];
+    int c,d,s;
+    cin>>c>>d>>s;
+    sort(a,a+n);
+		cout<<prec(10)<<(double)((double)(c-1)*(double)a[n-1])<<"\n";
 	}
-
 	return 0;
 }
